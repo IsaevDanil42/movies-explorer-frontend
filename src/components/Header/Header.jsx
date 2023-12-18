904d5b15763b1cd582ee7049b6afe373f124c802
Your Name <you@example.com>
2023-12-19 00:29:18 +0700

    feat: add header with logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,18 +3,27 @@ import icon from "../../images/profile-icon.svg";
 import "./Header.css";
 
 function Header() {
+  const loggedIn = true;
   return (
     <header className="header">
       <Link to="/" className="header__logo"></Link>
-      <nav className="header__menu">
-        <NavLink to="/movies" className={({ isActive }) => `header__menu-link ${isActive ? "header__menu-link_active" : ""}`}>Фильмы</NavLink>
-        <NavLink to="/saved-movies" className={({ isActive }) => `header__menu-link ${isActive ? "header__menu-link_active" : ""}`}>Сохранённые фильмы</NavLink>
-      </nav>
+      {loggedIn &&
+        <nav className="header__menu">
+          <NavLink to="/movies" className={({ isActive }) => `header__menu-link ${isActive ? "header__menu-link_active" : ""}`}>Фильмы</NavLink>
+          <NavLink to="/saved-movies" className={({ isActive }) => `header__menu-link ${isActive ? "header__menu-link_active" : ""}`}>Сохранённые фильмы</NavLink>
+        </nav>
+      }
       <div className="header__profile-container">
-        <Link to="/profile" className="header__profile-link">
-          <img src={icon}></img>
-          <span>Аккаунт</span>
-        </Link>
+        {loggedIn ?
+          <Link to="/profile" className="header__profile-link">
+            <img src={icon}></img>
+            <span>Аккаунт</span>
+          </Link> :
+          <>
+            <Link to="/signup" className="header__profile-signup">Регистрация</Link>
+            <Link to="/signin" className="header__profile-signin">Войти</Link>
+          </>
+        }
       </div>
     </header>
   );
