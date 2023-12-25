271001a490f0a448d27133e30e89f5eb109cf503
Daniil Isaev <shandydenn@yandex.ru>
2023-12-26 04:32:58 +0700

    feat: add routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,21 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="page">
       <Header></Header>
-      <Main></Main>
+      <Routes>
+        <Route path="/" element={<Main/>}></Route>
+        <Route path="/movies" element={<></>}></Route>
+        <Route path="/saved-movies" element={<></>}></Route>
+        <Route path="/profile" element={<></>}></Route>
+        <Route path="/signin" element={<></>}></Route>
+        <Route path="/signup" element={<></>}></Route>
+        <Route path="*" element={<></>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
