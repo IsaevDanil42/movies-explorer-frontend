ce8a3b313ddabea85641d5adefdc494f0fd3b659
Daniil Isaev <shandydenn@yandex.ru>
2023-12-26 02:10:47 +0700

    feat: add AboutMe  element

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -1,8 +1,21 @@
-import "./AboutMe.css"
+import "./AboutMe.css";
+import photo from "../../../images/my-photo.png";
 
 function AboutMe() {
-  return(
-    <section className="about-me" id="aboutMe"></section>
+  return (
+    <section className="about-me" id="aboutMe">
+      <h2 className="about-me__heading">Студент</h2>
+      <div className="about-me__container">
+        <div className="about-me__text-container">
+          <h3 className="about-me__title">Даниил</h3>
+          <p className="about-me__subtitle">Пока еще не фронтенд-разработчик, 25 лет</p>
+          <p className="about-me__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <a className="about-me__github" href="https://github.com/IsaevDanil42">Github</a>
+        </div>
+        <img className="about-me__photo" src={photo} alt="Моё фото"></img>
+      </div>
+    </section>
   );
 };
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,8 @@ import "./Main.css";
 import Promo from "./Promo/Promo";
 import NavTab from "./NavTab/NavTab";
 import AboutProject from "./AboutProject/AboutProject";
-import Techs from "./Techs/Techs"
+import Techs from "./Techs/Techs";
+import AboutMe from "./AboutMe/AboutMe";
 
 function Main() {
   return(
@@ -11,6 +12,7 @@ function Main() {
       <NavTab></NavTab>
       <AboutProject></AboutProject>
       <Techs></Techs>
+      <AboutMe></AboutMe>
     </main>
   );
 };
