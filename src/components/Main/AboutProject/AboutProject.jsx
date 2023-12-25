50652d93696b44e2bf8338a9d63ec6f4d1180e65
Daniil Isaev <shandydenn@yandex.ru>
2023-12-26 00:41:05 +0700

    feat: add AboutProject  element

--- src/components/Main/AboutProject/AboutProject.jsx
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -1,7 +1,29 @@
 import "./AboutProject.css";
 
 function AboutProject() {
-  return();
+  return (
+    <section className="about-project">
+      <h2 className="about-project__heading">О проекте</h2>
+      <div className="about-project__diplom-info">
+        <div className="about-project__description">
+          <h3 className="about-project__title">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </div>
+        <div className="about-project__description">
+          <h3 className="about-project__title">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+        <div className="about-project__timeline-backend">
+          <p className="about-project__timeline-backend-title">1 неделя</p>
+          <p className="about-project__timeline-backend-text">Back-end</p>
+        </div>
+        <div className="about-project__timeline-frontend">
+          <p className="about-project__timeline-frontend-title">4 недели</p>
+          <p className="about-project__timeline-frontend-text">Front-end</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AboutProject;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import "./Main.css";
 import Promo from "./Promo/Promo";
 import NavTab from "./NavTab/NavTab";
+import AboutProject from "./AboutProject/AboutProject";
 
 function Main() {
   return(
     <main>
       <Promo></Promo>
       <NavTab></NavTab>
+      <AboutProject></AboutProject>
     </main>
   );
 };
